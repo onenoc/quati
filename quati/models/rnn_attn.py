fixing some numerical stability issues

quati/models/rnn_attn.py
@@ -207,6 +207,8 @@ class RNNAttention(Model):
         assert self._loss is not None
 
         h = batch.words
+        if torch.isnan(h).any():
+            print('nan in word2vec')
         mask = h != constants.PAD_ID
         lengths = mask.int().sum(dim=-1)
 
@@ -218,12 +220,24 @@ class RNNAttention(Model):
         # (bs, ts) -> (bs, ts, emb_dim)
         self.embeddings_out = self.word_emb(h)
         h = self.dropout_emb(self.embeddings_out)
+        if torch.isnan(h).any():
+            print('nan after dropout embedding')
+        pre_rnn = h
 
         # (bs, ts, emb_dim) -> (bs, ts, hidden_size)
         h = pack(h, lengths.cpu(), batch_first=True, enforce_sorted=False)
         h, self.hidden = self.rnn(h, self.hidden)
         h, _ = unpack(h, batch_first=True)
 
+        if torch.isnan(h).any():
+            print('nan after packing, applying RNN, unpacking')
+            print(h)
+            print('pre-rnn is ')
+            print(pre_rnn)
+            print('params are')
+            print(self.rnn._parameters['weight_ih_l0'])
+            print(self.rnn._parameters['weight_hh_l0'])
+        
         # (dirs, bs, hidden_size) -> (bs, dirs*hidden_size)
         # q = self.hidden[0].transpose(0, 1).reshape(h.shape[0], -1)
 
@@ -241,12 +255,13 @@ class RNNAttention(Model):
         # apply dropout
         h = self.dropout_rnn(h)
 
+        if torch.isnan(h).any():
+            print('nan after dropout in RNN attention')
         # (bs, ts, hidden_size)  -> (bs, 1, hidden_size)
         h, self.attn_weights = self.attn(h, h, values=h, mask=mask)
 
         # (bs, 1, hidden_size) -> (bs, 1, nb_classes)
-        self.logits = self.linear_out(h)
-
+        self.logits = self.linear_out(h).clamp(max=5.,min=-5)
         # (bs, 1, nb_classes) -> (bs, 1, nb_classes) in log simplex
         h = F.log_softmax(self.logits, dim=-1)
 

quati/modules/basis_functions.py
@@ -234,7 +234,7 @@ class GaussianBasisFunctions(BasisFunctions):
         a = -5
         b = 5
         bandwidth=.01
-        dt_num = 1000
+        dt_num = 100
         t_raw = torch.linspace(a,b,dt_num,device=mu.device)
         t = t_raw.unsqueeze(0).unsqueeze(0)
         mu = mu.unsqueeze(-1)
@@ -245,14 +245,37 @@ class GaussianBasisFunctions(BasisFunctions):
         y2 = 1/(math.sqrt(2*math.pi)*sigma_basis)*torch.exp(-(mu_basis-t).pow(2)/(2*sigma_basis.pow(2)))
         dist_matrix = torch.cdist(torch.linspace(0,1,alpha.shape[1],device=mu.device).unsqueeze(-1),torch.linspace(a,b,dt_num,device=mu.device).unsqueeze(-1))
         K_matrix=torch.exp(-dist_matrix.pow(2)/(2*bandwidth))
-        y3 = torch.exp(torch.mm(alpha,K_matrix))
+        y3 = torch.exp(torch.mm(alpha,K_matrix)).clamp(max=100)
         y3 = y3.unsqueeze(1)
         #y3 = torch.ones(y3.shape,device=y3.device)
         Z = torch.trapz(y1*y3,torch.linspace(a,b,dt_num,device=mu.device),dim=-1).unsqueeze(-1)
         y = y1*y2*y3
+        if torch.isnan(y).any():
+            print('y1*y2*y3 is nan')
+            if torch.isnan(y3).any():
+                print('y3')
+                print(torch.mm(alpha,K_matrix))
+                if torch.isnan(torch.mm(alpha,K_matrix)).any():
+                    print('nan before exponentiating')
+                if torch.isnan(alpha).any():
+                    print('nan in alpha')
+                if torch.isnan(K_matrix).any():
+                    print('nan in kernel matrix')
+            if torch.isnan(y2).any():
+                print('y2')
+            if torch.isnan(y1).any():
+                print('y1')
+            print(5/0)
         y=y/Z
+        if torch.isnan(y).any():
+            print('y1*y2*y3/Z is nan')
+            print(torch.max(alpha))
+            print(torch.max(y3))
+            print(5/0)
         #Need normalization constant
         integral = torch.trapz(y,torch.linspace(a,b,dt_num,device=mu.device),dim=-1)
+        if torch.isnan(integral).any():
+            print('nan in integral')
         return integral
 
     def integrate_psi_gaussian(self, mu, sigma_sq):

quati/modules/continuous_attention.py
@@ -195,7 +195,7 @@ class ContinuousAttention(nn.Module):
             query, keys, mask=mask
         )
         self.sigma_sq = torch.clamp(self.sigma_sq, min=1e-6)
-        theta = torch.zeros(self.mu.size(0), 2, device=query.device)
+        theta = torch.zeros(self.mu.size(0), 12, device=query.device)
         theta[:, 0] = self.mu / self.sigma_sq
         theta[:, 1] = -1. / (2. * self.sigma_sq)
         theta[:, 2:] = alpha

quati/modules/continuous_encoders.py
@@ -98,7 +98,7 @@ class ConvEncoder(ContinuousEncoder):
     def __init__(self, vector_size, hidden_size, pool='max', supp_type='pred'):
         super().__init__()
         self.conv = nn.Conv1d(vector_size, hidden_size, kernel_size=3)
-        self.linear = nn.Linear(hidden_size, 2)
+        self.linear = nn.Linear(hidden_size, 12)
         self.pool = pool
         self.supp_type = supp_type
 
@@ -108,8 +108,15 @@ class ConvEncoder(ContinuousEncoder):
 
         # apply conv properly
         x = keys * mask.unsqueeze(-1).float() if mask is not None else keys
+
+        if torch.isnan(keys).any():
+            print('nan in keys')
+        if torch.isnan(x).any():
+            print('nan in continuous encoders from start')
         x = self.conv(x.transpose(-1, -2)).transpose(-1, -2)
         x = torch.relu(x)
+        if torch.isnan(x).any():
+            print('nan in continuous encoders after conv and relu')
 
         # pool a vector from the lstm states
         if self.pool == 'avg':
@@ -119,10 +126,14 @@ class ConvEncoder(ContinuousEncoder):
         elif self.pool == 'last':
             arange = torch.arange(x.shape[0]).to(x.device)
             x = x[arange, lengths - 1].squeeze(1)
+        if torch.isnan(x).any():
+            print('nan in continuous encoders after pooling')
 
         # use a hidden linear layer (query = param vector)
         # (bs, dirs*hidden_size) -> (bs, 2)
         x = self.linear(x)
+        if torch.isnan(x).any():
+            print('nan in continuous encoders after linear')
 
         # predict_mu with a sigmoid activation -> [0, 1]
         mu = torch.sigmoid(x[:, 0])

quati/modules/continuous_sparsemax.py
@@ -96,6 +96,12 @@ class ContinuousSparsemax(nn.Module):
     def truncated_parabola(self,t,mu,sigma_sq):
         return self.plus(-(t-mu)**2/(2*sigma_sq)+0.5*(3/(2*torch.sqrt(sigma_sq)))**(2./3.))
 
+    def gaussian_rbf(self,t,bandwidth):
+        return torch.exp(-0.5*t**2/bandwidth)
+
+    def beta_exp(self,t):
+        return self.plus(1+t)
+
     def forward(self, theta):
         #size: 1 x nb_basis x 1
         mu_basis = self.psi[0].mu.unsqueeze(-1)
@@ -103,23 +109,34 @@ class ContinuousSparsemax(nn.Module):
         a = -5
         b = 5
         dx = 100
+        bandwidth = 0.01
         #size: 1 x 1 x dx
-        T = torch.linspace(a,b,device=theta.device).unsqueeze(0).unsqueeze(0)
+        T = torch.linspace(a,b,dx,device=theta.device).unsqueeze(0).unsqueeze(0)
+        inducing_locations = torch.linspace(0,1,theta.shape[1]-2,device=theta.device)
+
+        K_inputs = torch.cdist(inducing_locations.unsqueeze(-1),torch.linspace(a,b,dx,device=theta.device).unsqueeze(-1))
+        #size: inducing x dx
+        K = self.gaussian_rbf(K_inputs,bandwidth)
+        alpha = theta[:,2:]
+        #size: bx x 1 x dx
+        f = torch.matmul(alpha,K).unsqueeze(1)
         #mu, sigma: bs x 1 x 1
         sigma = torch.sqrt(-0.5/theta[:,1])
         sigma_sq = (-0.5/theta[:,1]).unsqueeze(-1).unsqueeze(-1)
         mu = (theta[:,0]*sigma**2).unsqueeze(-1).unsqueeze(-1)
 
-
         phi1_upper = mu_basis-T
         phi1_lower = sigma_basis
         phi1 = self._phi(phi1_upper/phi1_lower)/phi1_lower
 
+        exp_terms = self.beta_exp(f-0.5*(mu-T)**2/(sigma_sq)).clamp(min=1e-8)
+
         #size: bs x 1 x dx
-        unnormalized_density = self.truncated_parabola(T,mu,sigma_sq)
+        #unnormalized_density = self.truncated_parabola(T,mu,sigma_sq)
         #size: bs x 1 x 1
-        Z = torch.trapz(unnormalized_density,torch.linspace(a,b,dx,device=theta.device),dim=-1).unsqueeze(-1)
-        numerical_integral = torch.trapz(phi1*unnormalized_density/Z,torch.linspace(a,b,dx,device=theta.device),dim=-1)
+        #Z = torch.trapz(unnormalizedi_density,torch.linspace(a,b,dx,device=theta.device),dim=-1).unsqueeze(-1)
+        Z = torch.trapz(exp_terms,torch.linspace(a,b,dx,device=theta.device),dim=-1).unsqueeze(-1)
+        numerical_integral = torch.trapz(phi1*exp_terms/Z,torch.linspace(a,b,dx,device=theta.device),dim=-1)
         return numerical_integral
 
 

quati/trainer.py
@@ -163,13 +163,15 @@ class Trainer:
 
     def _train(self):
         self.model.train()
+        torch.autograd.set_detect_anomaly(True)
         for i, batch in enumerate(self.train_iter, start=1):
 
             # basic training steps:
             self.model.zero_grad()
             pred = self.model(batch)
-            loss = self.model.loss(pred, batch.target)
+            loss = self.model.loss(pred, batch.target)#+2.5*(torch.norm(pred)**2)
             loss.backward()
+            torch.nn.utils.clip_grad_norm_(self.model.parameters(),0.1)
             self.optimizer.step()
 
             # keep stats object updated: